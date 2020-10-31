de9713dbaf92ed558a434b2973beb3dc9b607535
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-10-31 17:09:24 +0900

    fix migration_file

--- db/migrations/20201031015053_create_shipments_table.sql
+++ db/migrations/20201031015053_create_shipments_table.sql
@@ -4,9 +4,7 @@
 CREATE TABLE `shipments` (
     `id` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT '予約ID',
     `to_address` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT '配達先の住所',
-    `from_address` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT '配達元の住所',
-    `to_address` BIGINT COLLATE utf8mb4_bin NOT NULL COMMENT '配達先の住所',
-    `to_name` BIGINT COLLATE utf8mb4_bin NOT NULL COMMENT '配達先の名前',
+    `to_name` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT '配達先の名前',
     `from_address` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT '配達元の住所',
     `from_name` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT '配達元の名前',
     `status` varchar(127) COLLATE utf8mb4_bin NOT NULL COMMENT '配達ステータス',
