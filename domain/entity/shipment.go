cf52ca982c74331b9acf0915f4c868e53c925e65
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-10-31 17:41:24 +0900

    add domain

---
+++ domain/entity/shipment.go
@@ -0,0 +1,59 @@
+package entity
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Shipment struct {
+	ID              string    `json:"id" db:"id"`
+	ToAddress       string    `json:"to_address" db:"to_address"`
+	ToName          string    `json:"to_name" db:"to_name"`
+	FromAddress     string    `json:"from_address" db:"from_address"`
+	FromName        string    `json:"from_name" db:"from_name"`
+	Status          string    `json:"status" db:"status"`
+	ReserveDateTime time.Time `json:"reseve_date_time" db:"reserve_date_time"`
+	CreatedAt       time.Time `json:"created_at" db:"created_at"`
+}
+
+func NewShipment(toAddress string, toName string, fromAddress string, fromName string) (*Shipment, error) {
+	err := validateShipment(toAddress, toName, fromAddress, fromName)
+	if err != nil {
+		return nil, err
+	}
+	return &Shipment{
+		ID:              uuid.New().String(),
+		ToAddress:       toAddress,
+		ToName:          toName,
+		FromAddress:     fromAddress,
+		FromName:        fromName,
+		Status:          "initial",
+		ReserveDateTime: time.Now().AddDate(0, 0, 1), // 1日後
+		CreatedAt:       time.Now(),
+	}, nil
+}
+
+func validateShipment(toAddress string, toName string, fromAddress string, fromName string) error {
+	if toAddress == "" {
+		return fmt.Errorf("toAddress is required.")
+	}
+	if toName == "" {
+		return fmt.Errorf("toName is required.")
+	}
+	if fromAddress == "" {
+		return fmt.Errorf("fromAddress is required.")
+	}
+	if fromName == "" {
+		return fmt.Errorf("fromName is required.")
+	}
+	return nil
+}
+
+// 東京のタイムゾーンを取得
+//
+// @return *time.Location
+func Location() *time.Location {
+	return time.FixedZone("Asia/Tokyo", 9*60*60)
+}
