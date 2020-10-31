9c5bf749c7bbd51d0ec0e1dccc9dc6c5c84f1468
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-10-31 18:44:16 +0900

    add delete

--- domain/repository/shipment.go
+++ domain/repository/shipment.go
@@ -10,4 +10,5 @@ type ShipmentRepository interface {
 	FindByID(id string) (*entity.Shipment, error)
 	Store(ctx context.Context, shipment *entity.Shipment) (*entity.Shipment, error)
 	Update(ctx context.Context, shipment *entity.Shipment) (*entity.Shipment, error)
+	Delete(id string) error
 }
