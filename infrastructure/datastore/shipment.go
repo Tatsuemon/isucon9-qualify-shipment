09dc4e174ca9f3645ff7a42ddf9b62dec8010eb9
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-10-31 22:46:53 +0900

    fix nullstring, nulltime

--- infrastructure/datastore/shipment.go
+++ infrastructure/datastore/shipment.go
@@ -3,6 +3,7 @@ package datastore
 import (
 	"context"
 	"database/sql"
+	"time"
 
 	"github.com/Tatsuemon/isucon9-qualify-shipment/domain/entity"
 
@@ -19,10 +20,39 @@ func NewShipmentRepository(conn *sqlx.DB) repository.ShipmentRepository {
 	return &shipmentRepository{conn: conn}
 }
 
+type shipmentDTO struct {
+	ID              string         `db:"id"`
+	ToAddress       string         `db:"to_address"`
+	ToName          string         `db:"to_name"`
+	FromAddress     string         `db:"from_address"`
+	FromName        string         `db:"from_name"`
+	Status          string         `db:"status"`
+	ReserveDateTime time.Time      `db:"reserve_date_time"`
+	DoneDateTime    sql.NullTime   `db:"done_date_time"`
+	QRMD5           sql.NullString `db:"qrmd5"`
+	CreatedAt       time.Time      `db:"created_at"`
+}
+
 func (r *shipmentRepository) FindByID(id string) (*entity.Shipment, error) {
-	shipment := entity.Shipment{}
-	if err := r.conn.Get(&shipment, "SELECT id, to_address, to_name, from_address, from_name, status, reserve_date_time, done_date_time, qrmd5, created_at FROM shipments WHERE id = ?", id); err != nil {
+	dto := shipmentDTO{}
+	if err := r.conn.Get(&dto, "SELECT id, to_address, to_name, from_address, from_name, status, reserve_date_time, done_date_time, qrmd5, created_at FROM shipments WHERE id = ?", id); err != nil {
 		return nil, err
+	}
+	shipment := entity.Shipment{
+		ID:              dto.ID,
+		ToAddress:       dto.ToAddress,
+		ToName:          dto.ToName,
+		FromAddress:     dto.FromAddress,
+		FromName:        dto.FromName,
+		Status:          dto.Status,
+		ReserveDateTime: dto.ReserveDateTime,
+		CreatedAt:       dto.CreatedAt,
+	}
+	if dto.DoneDateTime.Valid {
+		shipment.DoneDateTime = dto.DoneDateTime.Time
+	}
+	if dto.QRMD5.Valid {
+		shipment.QRMD5 = dto.QRMD5.String
 	}
 
 	return &shipment, nil
@@ -48,7 +78,15 @@ func (r *shipmentRepository) Store(ctx context.Context, s *entity.Shipment) (*en
 		}
 	}()
 
-	_, err = stmt.Exec(s.ID, s.ToAddress, s.ToName, s.FromAddress, s.FromName, s.Status, s.ReserveDateTime, s.DoneDateTime, s.QRMD5, s.CreatedAt)
+	// time.Timeのゼロ値がnilでないため
+	var doneDateTime interface{}
+	if s.DoneDateTime.IsZero() {
+		doneDateTime = nil
+	} else {
+		doneDateTime = s.DoneDateTime
+	}
+
+	_, err = stmt.Exec(s.ID, s.ToAddress, s.ToName, s.FromAddress, s.FromName, s.Status, s.ReserveDateTime, doneDateTime, s.QRMD5, s.CreatedAt)
 	if err != nil {
 		return nil, err
 	}
@@ -75,7 +113,15 @@ func (r *shipmentRepository) Update(ctx context.Context, s *entity.Shipment) (*e
 		}
 	}()
 
-	_, err = stmt.Exec(s.ToAddress, s.ToName, s.FromAddress, s.FromName, s.Status, s.ReserveDateTime, s.DoneDateTime, s.QRMD5, s.CreatedAt, s.ID)
+	// time.Timeのゼロ値がnilでないため
+	var doneDateTime interface{}
+	if s.DoneDateTime.IsZero() {
+		doneDateTime = nil
+	} else {
+		doneDateTime = s.DoneDateTime
+	}
+
+	_, err = stmt.Exec(s.ToAddress, s.ToName, s.FromAddress, s.FromName, s.Status, s.ReserveDateTime, doneDateTime, s.QRMD5, s.CreatedAt, s.ID)
 	if err != nil {
 		return nil, err
 	}
