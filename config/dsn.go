9eab1618b94a3112e7e21b3b5fa89ffa8cc86e4b
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-10-31 23:40:25 +0900

    fix dsn

--- config/dsn.go
+++ config/dsn.go
@@ -8,10 +8,10 @@ import (
 func DSN() string {
 	return fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s",
-		os.Getenv("MYSQL_USER"),     // mysql
-		os.Getenv("MYSQL_PASSWORD"), // mysqlpass
-		os.Getenv("MYSQL_HOST"),     // db
-		os.Getenv("MYSQL_PORT"),     // 3306
-		os.Getenv("MYSQL_DATABASE"), // ddd_go
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DATABASE"),
 	) + "?parseTime=true&collation=utf8mb4_bin"
 }
